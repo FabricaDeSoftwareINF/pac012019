40d46471aa59753d8ddf939ccbdae18b4a7c1477
danilloguimaraes <danillo@danilloguimaraes.com.br>
2016-10-21 15:32:00 -0200

    Refatoração: Dependência não utilizada

--- pacDominio/src/main/java/br/ufg/inf/fabrica/pac/dominio/Estado.java
+++ pacDominio/src/main/java/br/ufg/inf/fabrica/pac/dominio/Estado.java
@@ -4,7 +4,6 @@ import br.ufg.inf.fabrica.pac.dominio.utils.UtilsValidacao;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
